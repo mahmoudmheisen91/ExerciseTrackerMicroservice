ef85723259bbdcf86e1d6ec7aaeebe60669fdf1a
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-03-17 11:23:18 +0200

    add greeting endpoint for testing

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,14 @@ const User = require("../models/user");
 let express = require("express");
 let router = express.Router();
 
+// Greeting endpoint...
+// GET Greeting: GET /api/exercise/hello
+router.get("/hello", (req, res) => {
+  res.json({ greetings: "Hello, API" });
+});
+
+// Create user endpoint...
+// POST Create users: POST /api/exercise/new-user
 router.post("/new-user", (req, res, next) => {
   let user = new User(req.body);
   user.save((err, userData) => {
